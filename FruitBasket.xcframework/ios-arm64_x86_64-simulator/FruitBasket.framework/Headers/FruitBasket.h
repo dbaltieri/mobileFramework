43b4fed9a2b260344b726076aa4f38551b2fc17e
dbaltieri <diegobaltieri@me.com>
2022-05-02 18:38:56 +0100

    It adds XCFramework

---
+++ FruitBasket.xcframework/ios-arm64_x86_64-simulator/FruitBasket.framework/Headers/FruitBasket.h
@@ -0,0 +1,18 @@
+//
+//  FruitBasket.h
+//  FruitBasket
+//
+//  Created by Diego Baltieri on 22/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FruitBasket.
+FOUNDATION_EXPORT double FruitBasketVersionNumber;
+
+//! Project version string for FruitBasket.
+FOUNDATION_EXPORT const unsigned char FruitBasketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruitBasket/PublicHeader.h>
+
+
